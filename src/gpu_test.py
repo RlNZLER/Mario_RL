028ec6d0f83af78fbee2639f48d5205c0d93e39b
RlNZLER <amelpvarghese@gmail.com>
2024-03-30 08:55:17 +0000

    CUDA installation scripts

--- src/gpu_test.py
+++ src/gpu_test.py
@@ -1,20 +1,20 @@
-import tensorflow as tf
+# import tensorflow as tf
 
-# Print out the available physical devices
-print("Available Physical Devices:")
-physical_devices = tf.config.list_physical_devices()
-for device in physical_devices:
-    print(device)
+# # Print out the available physical devices
+# print("Available Physical Devices:")
+# physical_devices = tf.config.list_physical_devices()
+# for device in physical_devices:
+#     print(device)
 
-# Print out the available GPUs
-print("\nAvailable GPUs:")
-gpus = tf.config.list_physical_devices('GPU')
-for gpu in gpus:
-    print(gpu)
+# # Print out the available GPUs
+# print("\nAvailable GPUs:")
+# gpus = tf.config.list_physical_devices('GPU')
+# for gpu in gpus:
+#     print(gpu)
 
-# Print out information about GPU usage by TensorFlow
-print("\nTensorFlow GPU Usage:")
-print(tf.config.experimental.list_logical_devices('GPU'))
+# # Print out information about GPU usage by TensorFlow
+# print("\nTensorFlow GPU Usage:")
+# print(tf.config.experimental.list_logical_devices('GPU'))
 
 
 import torch
@@ -22,3 +22,6 @@ import torchvision
 
 print("PyTorch version:", torch.__version__)
 print("Torchvision version:", torchvision.__version__)
+
+print(torch.cuda.is_available())
+print(torch.cuda.get_device_name(0))
