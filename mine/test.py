1432ee8de49b715da1fb42a723396cd37cc9de70
RlNZLER <amelpvarghese@gmail.com>
2024-04-15 00:18:56 +0100

    .

--- mine/test.py
+++ mine/test.py
@@ -1,173 +1,207 @@
-#####################################################
-# sb-SuperMarioBros.py
-#
-# This program trains deep reinforcement learning agents to solve the decision-making
-# problem of the the so-called LunarLander-v2. See details in the following link: 
-# https://www.gymlibrary.dev/environments/box2d/lunar_lander/
-#
-# Although the dependencies have been installed in the PCs of the labs. You may 
-# should be aware the following in case of installation in your own PCs:
-# pip install tensorflow[and-cuda]==2.15.1
-
-# pip install nes-py
-# pip install gym-super-mario-bros==7.3.0
-# pip install setuptools==65.5.0 "wheel<0.40.0"
-# pip install gym==0.21.0
-# pip install gymnasium
-# pip install stable-baselines3[extra]==1.8.0
-# pip install sb3_contrib==1.8.0
-
-# Version: 1.0 -- extended version from sb-LunarLander.py
-# Date: 10 March 2024
-# Contact: hcuayahuitl@lincoln.ac.uk
-#####################################################
-
+import warnings
 import csv
-import sys
-import gym
-import time 
-import pickle
+import time
+import torch
+import optuna
 import random
 import datetime
-import subprocess
-import webbrowser
-from typing import Callable
-from stable_baselines3 import DQN,A2C,PPO
+from optuna.visualization import (
+    plot_optimization_history,
+    plot_param_importances,
+    plot_slice,
+)
+from stable_baselines3 import DQN, A2C, PPO
 from stable_baselines3.common.evaluation import evaluate_policy
 import gym_super_mario_bros
 from nes_py.wrappers import JoypadSpace
-from gym_super_mario_bros.actions import SIMPLE_MOVEMENT, COMPLEX_MOVEMENT, RIGHT_ONLY
-from stable_baselines3.common import atari_wrappers
-
-# Path to the TensorBoard log directory
-tensorboard_log_dir = "tensorboard_log"
-
-# Start TensorBoard as a background process
-tensorboard_process = subprocess.Popen(
-    ['tensorboard', '--logdir', tensorboard_log_dir],
-    stdout=subprocess.PIPE, stderr=subprocess.PIPE
+from gym_super_mario_bros.actions import RIGHT_ONLY
+from stable_baselines3.common.atari_wrappers import (
+    MaxAndSkipEnv,
+    NoopResetEnv,
+    ClipRewardEnv,
 )
 
-# Optionally, open a web browser automatically pointing to TensorBoard
-webbrowser.open("http://localhost:6006/", new=2)
+warnings.filterwarnings("ignore", category=RuntimeWarning)
 
-print("TensorBoard is running. Visit http://localhost:6006/ in your web browser.")
+# Global settings
+N_TRIALS = 50
+N_TIMESTEPS = int(2e4)
+N_EVAL_EPISODES = 5
+ENV_ID = "SuperMarioBros2-v1"
+ALGORITHMS = ["DQN", "A2C", "PPO"]  # Algorithms to test
 
-# It's generally a good idea to give TensorBoard a few seconds to start up before the training starts
-time.sleep(5)
-
-def log_training_results(algorithm, seed, learning_rate, gamma, num_training_steps, mean_reward, std_reward, avg_game_score, training_time, testing_time):
-    # log training results to CSV
-    log_filename = "training_log.csv"
-    
-    # Get current timestamp
+def log_training_results(algorithm, hyperparameters, mean_reward, std_reward, training_time):
+    log_filename = "hyperparameter_log_v3.csv"
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    
-    # Write results to CSV
-    with open(log_filename, mode='a', newline='') as file:
+    with open(log_filename, mode="a", newline="") as file:
         writer = csv.writer(file)
-        # Check if the file is empty
         file.seek(0, 2)
         if file.tell() == 0:
-            # If empty, write header
-            writer.writerow(["Timestamp", "Algorithm", "Seed", "Learning Rate","Gamma", "No. of training steps", "Mean Reward", "Std Reward", "Avg Game Score", "Training Time", "Testing Time"])
-        # Write data to CSV
-        writer.writerow([timestamp, algorithm, seed, learning_rate, gamma, num_training_steps, mean_reward, std_reward, avg_game_score, training_time, testing_time])
+            writer.writerow([
+                "Timestamp",
+                "Algorithm",
+                "Hyperparameters",
+                "Mean Reward",
+                "Std Reward",
+                "Training Time",
+            ])
+        writer.writerow([
+            timestamp,
+            algorithm,
+            str(hyperparameters),
+            mean_reward,
+            std_reward,
+            training_time,
+        ])
+        
+def calculate_batch_size(n_steps, min_batch_size=8, max_batch_size=256):
+    total_steps = n_steps
+    # Start with the max batch size and decrease until finding a divisor or reaching the min batch size
+    for potential_batch_size in range(max_batch_size, min_batch_size - 1, -1):
+        if total_steps % potential_batch_size == 0:
+            return potential_batch_size
+    # If no divisor is found within bounds, return the minimum batch size
+    return min_batch_size
 
-if len(sys.argv)<2 or len(sys.argv)>4:
-    print("USAGE: sb-SuperMarioBros2-v1.py (train|test) (DQN|A2C|PPO) [seed_number]")
-    exit(0)
-
-environmentID = "SuperMarioBros2-v1"
-trainMode = True if sys.argv[1] == 'train' else False
-learningAlg = sys.argv[2] 
-seed = random.randint(0,1000) if trainMode else int(sys.argv[3])
-policyFileName = "policy/"+learningAlg+"-"+environmentID+"-seed"+str(seed)+".policy.pkl"
-num_training_steps = 50_000
-num_test_episodes = 10
-learning_rate = 0.0008545550270720303
-gamma = 0.82871784570218
-policy_rendering = True
-
-# create the learning environment 
 def make_env(gym_id, seed):
     env = gym_super_mario_bros.make(gym_id)
     env = JoypadSpace(env, RIGHT_ONLY)
-    env = atari_wrappers.MaxAndSkipEnv(env, 4)
-    env = atari_wrappers.NoopResetEnv(env, noop_max=30)
-    env = atari_wrappers.ClipRewardEnv(env)
-    env.seed(seed)	
+    env = MaxAndSkipEnv(env, 4)
+    env = NoopResetEnv(env, noop_max=30)
+    env = ClipRewardEnv(env)
+    env.seed(seed)
     env.action_space.seed(seed)
     env.observation_space.seed(seed)
     return env
 
-environment = make_env(environmentID, seed)
+def objective(trial, algorithm):
+    # Specify hyperparameters based on the algorithm
+    hyperparameters = {
+        "learning_rate": trial.suggest_float("learning_rate", 1e-5, 1e-2, log=True),
+        "gamma": trial.suggest_float("gamma", 0.8, 0.9999),
+        "buffer_size": None,
+        "batch_size": None,
+        "train_freq": None,
+        "target_update_interval": None,
+        "exploration_fraction": None,
+        "exploration_final_eps": None,
+        "n_steps": None,
+        "vf_coef": None,
+        "ent_coef": None,
+        "n_epochs": None
+    }
 
-# create the agent's model using one of the selected algorithms
-# note: exploration_fraction=0.9 means that it will explore 90% of the training steps
-if learningAlg == "DQN":
-    model = DQN("CnnPolicy", environment, seed=seed, learning_rate=learning_rate, gamma=gamma, buffer_size=50000, exploration_fraction=0.9, verbose=1, tensorboard_log="tensorboard_log/DQN_tensorboard_log")
-elif learningAlg == "A2C":
-    model = A2C("CnnPolicy", environment, seed=seed, learning_rate=learning_rate, gamma=gamma, verbose=1, tensorboard_log="tensorboard_log/A2C_tensorboard_log")
-elif learningAlg == "PPO":
-    model = PPO("CnnPolicy", environment, seed=seed, learning_rate=learning_rate, gamma=gamma, verbose=1, tensorboard_log="tensorboard_log/PPO_tensorboard_log")
-else:
-    print("UNKNOWN learningAlg="+str(learningAlg))
-    exit(0)
+    # Initialize environment
+    env = make_env(ENV_ID, random.randint(0, 1000))
 
-# train the agent or load a pre-trained one
-if trainMode:
-    start_time = time.time()  # Start measuring training time
-    model.learn(total_timesteps=num_training_steps, progress_bar=True)
-    
-    end_time = time.time() # Stop measuring training time
-    training_time = end_time - start_time  # Calculate training time
-    
-    print("Saving policy "+str(policyFileName))
-    pickle.dump(model.policy, open(policyFileName, 'wb'))
-else:
-    print("Loading policy...")
-    with open(policyFileName, "rb") as f:
-        policy = pickle.load(f)
-    model.policy = policy
+    # Create and return model based on the algorithm
+    if algorithm == "DQN":
+        hyperparameters.update({
+            "buffer_size": trial.suggest_int("buffer_size", 10000, 100000),
+            "batch_size": trial.suggest_int("batch_size", 32, 256),
+            "train_freq": trial.suggest_int("train_freq", 1, 100),
+            "target_update_interval": trial.suggest_int("target_update_interval", 100, 5000),
+            "exploration_fraction": trial.suggest_float("exploration_fraction", 0.1, 0.5),
+            "exploration_final_eps": trial.suggest_float("exploration_final_eps", 0.01, 0.1)
+        })
 
-print("Evaluating policy...")
-mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=num_test_episodes*5)
-print("EVALUATION: mean_reward=%s std_reward=%s" % (mean_reward, std_reward))
+        model = DQN(
+            "CnnPolicy", 
+            env,
+            verbose=0,
+            learning_rate=hyperparameters["learning_rate"],
+            buffer_size=hyperparameters["buffer_size"],
+            learning_starts=1000,
+            batch_size=hyperparameters["batch_size"],
+            gamma=hyperparameters["gamma"],
+            train_freq=hyperparameters["train_freq"],
+            target_update_interval=hyperparameters["target_update_interval"],
+            exploration_fraction=hyperparameters["exploration_fraction"],
+            exploration_final_eps=hyperparameters["exploration_final_eps"],
+            tensorboard_log=None
+        )
+    elif algorithm == "A2C":
+        hyperparameters.update({
+            "n_steps": trial.suggest_int("n_steps", 5, 256),
+            "vf_coef": trial.suggest_float("vf_coef", 0.1, 1.0),
+            "ent_coef": trial.suggest_float("ent_coef", 0.0001, 0.1)
+        })
 
-# visualise the agent's learnt behaviour.
-data_batch_size = 1000  # Write to the file every 1000 episodes
-steps_per_episode = 0
-reward_per_episode = 0
-total_cummulative_reward = 0
-episode = 1
-start_time = time.time()  # Start measuring testing time.
-env = model.get_env()
-obs = env.reset()
-while True and policy_rendering:
-    action, _states = model.predict(obs, deterministic=True)
-    obs, reward, done, info = env.step(action)
-    steps_per_episode += 1
-    reward_per_episode += reward
-    total_game_score = info[0]['score']
-    if any(done):
-        print("episode=%s, steps_per_episode=%s, reward_per_episode=%s, total_game_score=%s" % (episode, steps_per_episode, reward_per_episode, total_game_score))
-        total_cummulative_reward += reward_per_episode
-        steps_per_episode = 0
-        reward_per_episode = 0
-        episode += 1
-        obs = env.reset()
-    env.render("human")
-    if episode >= num_test_episodes:
-        avg_cummulative_reward = total_cummulative_reward/num_test_episodes
-        avg_game_score = total_game_score/num_test_episodes
-        print("total_cummulative_reward=%s avg_cummulative_reward=%s avg_game_score=%s" % \
-              (total_cummulative_reward, avg_cummulative_reward, avg_game_score))
-        policy_rendering = False
-        break
-env.close()
-end_time = time.time() # Stop measuring testing time.
-testing_time = end_time - start_time  # Calculate training time.
+        model = A2C(
+            "CnnPolicy", 
+            env,
+            verbose=0,
+            learning_rate=hyperparameters["learning_rate"],
+            n_steps=hyperparameters["n_steps"],
+            gamma=hyperparameters["gamma"],
+            vf_coef=hyperparameters["vf_coef"],
+            ent_coef=hyperparameters["ent_coef"],
+            tensorboard_log=None
+        )
+    elif algorithm == "PPO":
+        hyperparameters.update({
+            "n_steps": trial.suggest_int("n_steps", 64, 2048, log=True),
+            "ent_coef": trial.suggest_float("ent_coef", 0.0001, 0.1, log=True),
+            "n_epochs": trial.suggest_int("n_epochs", 1, 10),
+        })
+        hyperparameters["batch_size"] = calculate_batch_size(hyperparameters["n_steps"])
 
-if trainMode:
-    log_training_results(learningAlg, seed, learning_rate, gamma, num_training_steps, mean_reward, std_reward, avg_game_score, training_time, testing_time)
+        model = PPO(
+            "CnnPolicy",
+            env,
+            verbose=0,
+            learning_rate=hyperparameters["learning_rate"],
+            n_steps=hyperparameters["n_steps"],
+            gamma=hyperparameters["gamma"],
+            ent_coef=hyperparameters["ent_coef"],
+            n_epochs=hyperparameters["n_epochs"],
+            batch_size=hyperparameters["batch_size"],
+            tensorboard_log=None
+        )
+    else:
+        raise ValueError("Unsupported algorithm specified.")
+
+    # Learn and evaluate policy
+    try:
+        start_time = time.time()
+        model.learn(total_timesteps=N_TIMESTEPS)
+        mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=N_EVAL_EPISODES, return_episode_rewards=False)
+        training_time = time.time() - start_time
+    finally:
+        # Clean up: Ensure all GPU memory is freed.
+        env.close()
+        model = None
+        torch.cuda.empty_cache()
+
+    # Log the training results including all hyperparameters
+    log_training_results(algorithm, hyperparameters, mean_reward, std_reward, training_time)
+
+    return mean_reward
+
+if __name__ == "__main__":
+    overall_best_reward = -float('inf')
+    best_algorithm = None
+
+    for algorithm in ALGORITHMS:
+        study = optuna.create_study(direction="maximize", pruner=optuna.pruners.MedianPruner(n_warmup_steps=5))
+        study.optimize(lambda trial: objective(trial, algorithm), n_trials=N_TRIALS, show_progress_bar=True)
+
+        # Output results
+        print(f"Results for {algorithm}:")
+        print(f"Best trial until now: {study.best_trial.value}")
+        print(f"Best hyperparameters: {study.best_trial.params}")
+
+        # Update the best algorithm based on the reward
+        if study.best_trial.value > overall_best_reward:
+            overall_best_reward = study.best_trial.value
+            best_algorithm = algorithm
+
+        # Visualization (optional)
+        optimization_history = plot_optimization_history(study)
+        optimization_history.show()
+        param_importances = plot_param_importances(study)
+        param_importances.show()
+        slice_plot = plot_slice(study)
+        slice_plot.show()
+
+    print(f"The best performing algorithm is {best_algorithm} with a reward of {overall_best_reward}")
